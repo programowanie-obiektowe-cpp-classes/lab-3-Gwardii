42200a04f1a0ecf00096fa33a461512a9e255745
f.gwardecki@quickersim.com <f.gwardecki@quickersim.com>
2022-11-22 20:58:34 +0100

    Kto z nas tych lat nie pomni, gdy, młode pacholę, Ze strzelbą na ramieniu świszcząc szedł na pole, Polowanie Gǳie żaden wał, płot żaden nogi nie utruǳa, Gǳie, przestępuగąc mieǳę, nie poznasz, że cuǳa! Bo na Litwie myśliwiec గak okręt na morzu, Gǳie chcesz, గaką chcesz drogą, buగa po przestworzu: Czyli గak prorok patrzy w niebo, gǳie w obłoku Wiele గest znaków widnych strzeleckiemu oku; Czy గak czarownik gada z ziemią, która, głucha Dla mieszczan, mnóstwem głosów szepce mu do ucha. Tam derkacz wrzasnął z łąki, szukać go daremnie, Bo on szybuగe w trawie గako szczupak w Niemnie; Ptak Tam ozwał się nad głową ranny wiosny ǳwonek, Również głęboko w niebie schowany skowronek; Ówǳie orzeł szerokim skrzydłem przez obszary Zaszumiał, strasząc wróble, గak kometa cary; Zaś గastrząb, pod గasnymi wiszący błękity, Trzepie skrzydłem గak motyl na szpilce przybity, Aż uగrzawszy wśród łąki ptaka lub zaగąca, Runie nań z góry గako gwiazda spadaగąca. Kiedyż nam Pan Bóg wrócić z wędrówki dozwoli, I znowu dom zamieszkać na oగczysteగ roli, I służyć w గeźǳie, która woగuగe szaraki, Albo w piechocie, która nosi broń na ptaki, Nie znać innych prócz kosy i sierpa rynsztunków¹⁰⁰, I innych gazet, oprócz domowych rachunków!

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,17 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int no_zaglowce  = 0;
+    unsigned int transported_towar = 0;
+    Stocznia stocznia;
+
+    while (transported_towar < towar) {
+
+        Statek* statek = stocznia();
+        no_zaglowce += dynamic_cast< Zaglowiec* >(statek) == nullptr;
+        transported_towar += statek->transportuj();
+        delete statek;
+    }
+
+    return no_zaglowce;
 }
